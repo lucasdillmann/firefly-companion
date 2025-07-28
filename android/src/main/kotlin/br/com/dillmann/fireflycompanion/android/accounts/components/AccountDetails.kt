57826023df3521ac4d2923168088059ef76df205
Lucas Dillmann <lucas@dillmann.com.br>
2025-07-27 22:08:57 -0300

    Minor improvements

--- android/src/main/kotlin/br/com/dillmann/fireflycompanion/android/accounts/components/AccountDetails.kt
+++ android/src/main/kotlin/br/com/dillmann/fireflycompanion/android/accounts/components/AccountDetails.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -16,6 +17,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -119,6 +121,7 @@ fun AccountDetails(
                     fontSize = LocalTextStyle.current.fontSize.times(2),
                 ),
                 singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
             )
 
             Section(
